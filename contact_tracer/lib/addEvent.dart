cb10a2700aa815cb5c90794e99286fd3f8d6a30b
Mike <michaeldtompkins@hotmail.com>
2020-04-18 19:28:37 -0400

    Redesign

--- contact_tracer/lib/addEvent.dart
+++ contact_tracer/lib/addEvent.dart
@@ -1,6 +1,7 @@
 import 'package:contact_tracer/event.dart';
 import 'package:flutter/material.dart';
 import 'addDateAndTime.dart';
+import 'dart:collection';
 
 class AddEvent extends StatefulWidget {
   Function addEventToList;
@@ -14,12 +15,30 @@ class AddEvent extends StatefulWidget {
 }
 
 class _AddEventState extends State<AddEvent> {
-  static String _inputLocation;
-  static String _inputPerson;
-  static DateTime _inputDate;
-  static TimeOfDay _inputTime;
+  String _inputLocation;
+  DateTime _inputDate;
+  TimeOfDay _inputTime;
 
-  static void updateInputDateAndTime({DateTime inputDate, TimeOfDay inputTime}) {
+  // Use a SplayTreeMap so that items are sorted by key
+  SplayTreeMap<String, bool> _people = new SplayTreeMap<String, bool>();
+  static final String _defaultDisplayPersons = 'Person(s)';
+  String _displayPersons = _defaultDisplayPersons;
+  
+  AddDateAndTime dateAndTime;
+
+  _AddEventState() {
+    dateAndTime = new AddDateAndTime(_updateInputDateAndTime);
+
+    // Default list for testing
+    _people['Mike'] = true;
+    _people['Dylan'] = false;
+
+    // Configure the default people lable
+    _setDisplayPersons();
+  }
+
+  void _updateInputDateAndTime(
+      {DateTime inputDate, TimeOfDay inputTime}) {
     if (inputTime != null) {
       _inputTime = inputTime;
     }
@@ -29,7 +48,29 @@ class _AddEventState extends State<AddEvent> {
     }
   }
 
-  AddDateAndTime dateAndTime = new AddDateAndTime(updateInputDateAndTime);
+  void _setDisplayPersons() {
+    // Default to blank
+    _displayPersons = '';
+
+    // Build comma separated list
+    _people.forEach((name, checked) {
+      if (checked)
+        _displayPersons += ((_displayPersons.length > 0) ? ', ' : '') + name;
+    });
+
+    // If no people are selected, use the default label
+    if (_displayPersons.length == 0)
+      _displayPersons = _defaultDisplayPersons;
+  }
+
+  bool _peopleExist() {
+    _people.forEach((name, checked) {
+      if (checked)
+        return true;
+    });
+
+    return false;
+  }
 
   @override
   Widget build(BuildContext _context) {
@@ -45,74 +86,39 @@ class _AddEventState extends State<AddEvent> {
       ),
     );
 
-    var inputPersonTextEditingController = TextEditingController(
-      text: (_inputPerson != null) ? _inputPerson : ''); // Configure text to a non-null value
-
-    void _showMultiSelectDialog() async {
-      Set<int> initialSelectedValues = Set();
-      List<MultiSelectDialogItem<int>> items = <MultiSelectDialogItem<int>>[
-        MultiSelectDialogItem(0, 'Dog'),
-        MultiSelectDialogItem(1, 'Cat'),
-        MultiSelectDialogItem(2, 'Mouse'),
-      ];
-
-      // Eliminate non-empty, blank line
-      inputPersonTextEditingController.text = inputPersonTextEditingController.text.trim();
-
-      // If input already exists, add to the list
-      if (inputPersonTextEditingController.text.length > 0) {
-        items.insert(
-          items.length, 
-          MultiSelectDialogItem(items.length, inputPersonTextEditingController.text));
-      }
-
-      // Present the list in sorted order for easy perusal
-      items.sort((a, b) => a.label.compareTo(b.label));
-      for (var i = 0; i < items.length; ++i) {
-        items[i].index = i;
-      }
-
-      // Select inputted value by default
-      for (var i in items) {
-        if (i.label == inputPersonTextEditingController.text) 
-          initialSelectedValues.add(i.index);
-      }
-
-      final selectedValues = await showDialog<Set<int>>(
-        context: context,
-        builder: (BuildContext context) {
-          return MultiSelectDialog(
-            items: items,
-            initialSelectedValues: initialSelectedValues,
-          );
-        },
-      );
-
-      if ((selectedValues != null) && (selectedValues.length > 0)) {
-        inputPersonTextEditingController.text = '';
-        print('Selected values:');
-        for (var i in selectedValues) {
-            dynamic person = items.elementAt(i).label;
-            print(person);
-            inputPersonTextEditingController.text += 
-              ((inputPersonTextEditingController.text.length > 0) ? ', ' : '') + person;
-        }
-      }
-    }
-
     Card person = Card(
-      child: ListTile(
-        leading: Icon(Icons.person),
-        title: TextField(
-          decoration: InputDecoration(labelText: 'Person'),
-          controller: inputPersonTextEditingController,
-          onTap: () {
-              _showMultiSelectDialog();
-          },
-          onChanged: (String value) {
-            _inputPerson = value;
-          },
-        ),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          Row(
+            children: <Widget>[
+              Expanded(
+                child: ListTile(
+                  leading: Icon(Icons.person),
+                  title: Text(_displayPersons),
+                ),
+              ),
+              IconButton(
+                icon: Icon(Icons.add_circle_outline),
+                onPressed: () {},
+              ),
+            ],
+          ),
+          new ListView(
+              shrinkWrap: true,
+              children: _people.keys.map((String name) {
+                return new CheckboxListTile(
+                  title: new Text(name),
+                  value: _people[name],
+                  onChanged: (bool checked) {
+                    setState(() {
+                      _people[name] = checked;
+                      _setDisplayPersons();
+                    });
+                  },
+                );
+              }).toList()),
+        ],
       ),
     );
 
@@ -134,11 +140,11 @@ class _AddEventState extends State<AddEvent> {
       );
 
       if ((_inputLocation != null) &&
-          (_inputPerson != null) &&
+          _peopleExist() &&
           (_inputDate != null) &&
           (_inputTime != null)) {
         Event e =
-            new Event(_inputLocation, _inputPerson, _inputDate, _inputTime);
+            new Event(_inputLocation, _people, _inputDate, _inputTime);
         widget.addEventToList(e);
         Navigator.pop(context);
       } else {
@@ -164,8 +170,8 @@ class _AddEventState extends State<AddEvent> {
       body: ListView(
         children: [
           location,
-          person,
           dateAndTime,
+          person,
         ],
       ),
       floatingActionButton: FloatingActionButton(
@@ -176,84 +182,7 @@ class _AddEventState extends State<AddEvent> {
   }
 }
 
-class MultiSelectDialogItem<V> {
-  MultiSelectDialogItem(this.index, this.label);
-
-  V index;
-  final String label;
-}
-
-class MultiSelectDialog<V> extends StatefulWidget {
-  MultiSelectDialog({Key key, this.items, this.initialSelectedValues}) : super(key: key);
-
-  final List<MultiSelectDialogItem<V>> items;
-  final Set<V> initialSelectedValues;
-
+class AlwaysDisabledFocusNode extends FocusNode {
   @override
-  State<StatefulWidget> createState() => _MultiSelectDialogState<V>();
-}
-
-class _MultiSelectDialogState<V> extends State<MultiSelectDialog<V>> {
-  final _selectedValues = Set<V>();
-
-  void initState() {
-    super.initState();
-    if (widget.initialSelectedValues != null) {
-      _selectedValues.addAll(widget.initialSelectedValues);
-    }
-  }
-
-  void _onItemCheckedChange(V itemValue, bool checked) {
-    setState(() {
-      if (checked) {
-        _selectedValues.add(itemValue);
-      } else {
-        _selectedValues.remove(itemValue);
-      }
-    });
-  }
-
-  void _onCancelTap() {
-    Navigator.pop(context);
-  }
-
-  void _onSubmitTap() {
-    Navigator.pop(context, _selectedValues);
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return AlertDialog(
-      title: Text('Select animals'),
-      contentPadding: EdgeInsets.only(top: 12.0),
-      content: SingleChildScrollView(
-        child: ListTileTheme(
-          contentPadding: EdgeInsets.fromLTRB(14.0, 0.0, 24.0, 0.0),
-          child: ListBody(
-            children: widget.items.map(_buildItem).toList(),
-          ),
-        ),
-      ),
-      actions: <Widget>[
-        FlatButton(
-          child: Text('CANCEL'),
-          onPressed: _onCancelTap,
-        ),
-        FlatButton(
-          child: Text('OK'),
-          onPressed: _onSubmitTap,
-        )
-      ],
-    );
-  }
-
-  Widget _buildItem(MultiSelectDialogItem<V> item) {
-    final checked = _selectedValues.contains(item.index);
-    return CheckboxListTile(
-      value: checked,
-      title: Text(item.label),
-      controlAffinity: ListTileControlAffinity.leading,
-      onChanged: (checked) => _onItemCheckedChange(item.index, checked),
-    );
-  }
+  bool get hasFocus => false;
 }
